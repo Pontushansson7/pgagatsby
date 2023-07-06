0f65bf57d89fb6e8f4dae003c2c5de52408e74e4
Pontus Hansson <pontushansson7@gmail.com>
2023-07-06 20:02:18 +0200

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -72,7 +72,7 @@ const utmParameters = `?utm_source=starter&utm_medium=start-page&utm_campaign=de
 const IndexPage = () => (
   <Layout>
     <div className={styles.textCenter}>
-        HElloooss
+        HEllooos
     </div>
     
     {moreLinks.map((link, i) => (
