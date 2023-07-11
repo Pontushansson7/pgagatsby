70fbce2667f68c37ae925a77a6cc34832d8957a0
Pontus Hansson <pontushansson7@gmail.com>
2023-07-11 08:07:34 +0200

    new site

--- src/pages/index.js
+++ src/pages/index.js
@@ -72,7 +72,7 @@ const utmParameters = `?utm_source=starter&utm_medium=start-page&utm_campaign=de
 const IndexPage = () => (
   <Layout>
     <div className={styles.textCenter}>
-        Send 10 dollars and amanda will display tits.
+        New site, with deploy setup!
     </div>
     
     {moreLinks.map((link, i) => (
